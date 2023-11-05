092fd21a3e284eb61999ad6be0ffb7f2dcee65ea
Sam Ivanov <si@juno-solar.com>
2023-11-05 15:14:24 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            double number1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double number2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter an arithmetic operation (+, -, *, /): ");
+            char operation = char.Parse(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = number1 + number2;
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    break;
+                case '/':
+                    if (number2 != 0)
+                    {
+                        result = number1 / number2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero.");
+                        return;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error: Invalid operation.");
+                    return;
+            }
+
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
